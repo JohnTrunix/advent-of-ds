09e0ff3569e20f9eb7239291a575f7361e662db8
JohnTrunix <john@trunix.ch>
2023-12-10 12:34:42 +0100

    feat(calendar): init calendar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
 import { AboutComponent } from './pages/about/about.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'calendar', component: CalendarComponent },
     { path: 'leaderboard', component: LeaderboardComponent },
     { path: 'about', component: AboutComponent },
     { path: 'login', redirectTo: 'home', pathMatch: 'full' },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
 
 @NgModule({
     declarations: [
@@ -15,6 +16,7 @@ import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component'
         AboutComponent,
         NavbarComponent,
         LeaderboardComponent,
+        CalendarComponent,
     ],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
