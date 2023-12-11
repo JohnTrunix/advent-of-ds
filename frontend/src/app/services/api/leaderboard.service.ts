76e92c44a634c9f870d9f3b042986c6c3f40f195
JohnTrunix <john@trunix.ch>
2023-12-12 00:15:30 +0100

    feat(leaderboard): add leaderboard api service

---
+++ frontend/src/app/services/api/leaderboard.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
+import { LeaderboardModel } from '../../models/leaderboard.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class LeaderboardService {
+    private leaderboardUrl = environment.apiBaseUrl + '/leaderboard';
+    httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+    constructor(private http: HttpClient) {}
+
+    /**
+     * Get leaderboard
+     */
+    getLeaderboard(): Observable<LeaderboardModel> {
+        return this.http.get<LeaderboardModel>(this.leaderboardUrl).pipe(
+            catchError(
+                this.handleError<LeaderboardModel>('getLeaderboard', {
+                    entries: [],
+                    request_time: new Date(),
+                })
+            )
+        );
+    }
+
+    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: ErrorEvent): Observable<T> => {
+            console.error(operation + ' failed');
+            console.error(error);
+            return of(result as T);
+        };
+    }
+}
