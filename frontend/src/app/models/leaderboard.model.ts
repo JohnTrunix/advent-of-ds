0ffcb26c7bf71a4240479378cca86840fc71d82b
JohnTrunix <john@trunix.ch>
2023-12-12 00:15:20 +0100

    feat(leaderboard): add leaderboard model

--- frontend/src/app/models/leaderboard.model.ts
+++ frontend/src/app/models/leaderboard.model.ts
@@ -1,6 +1,11 @@
-export interface LeaderboardModel {
+export interface LeaderboardEntryModel {
     id: number;
     name: string;
     score: number;
     coffees: number;
+}
+
+export interface LeaderboardModel {
+    entries: LeaderboardEntryModel[];
+    request_time: Date;
 }
