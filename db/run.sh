0831a3f5d2d569aed7f22dc339ff06f88356a69b
JohnTrunix <john@trunix.ch>
2023-12-19 11:11:10 +0100

    chore(db): rm old container before generating new

--- db/run.sh
+++ db/run.sh
@@ -1,2 +1,7 @@
+# stop and remove container
+docker stop aods-database
+docker rm aods-database
+
+# build and run container
 docker build -t pg-database .
 docker run -p 5432:5432 --name aods-database -d pg-database
