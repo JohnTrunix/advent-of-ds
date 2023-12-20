d625917fb5fe06951b9aa7187b44da4db6c9fafa
JohnTrunix <john@trunix.ch>
2023-12-20 22:57:31 +0100

    fix(backend): add CORS middleware for frontend access

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes import leaderboard
 
@@ -10,6 +11,10 @@ class Tags(Enum):
     leaderboard = "leaderboard"
 
 
+origins: list[str] = [
+    "http://localhost:4200",
+]
+
 app: FastAPI = FastAPI(
     title="Advent of Data Science",
     description="Backend for the Advent of Data Science (AoDS) challenge.",
@@ -18,6 +23,13 @@ app: FastAPI = FastAPI(
         {"name": Tags.root, "description": "Root endpoint"},
         {"name": Tags.leaderboard, "description": "Leaderboard endpoint"},
     ],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET"],
+    allow_headers=["*"],
 )
 
 app.include_router(leaderboard.router, prefix="/v1", tags=[Tags.leaderboard])
