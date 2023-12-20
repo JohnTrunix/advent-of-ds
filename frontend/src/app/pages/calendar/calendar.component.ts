bfa58b4764c26e7149b36542373a6d8a0f50d7fd
JohnTrunix <john@trunix.ch>
2023-12-20 23:42:10 +0100

    feat(frontend): add api to calendar

--- frontend/src/app/pages/calendar/calendar.component.ts
+++ frontend/src/app/pages/calendar/calendar.component.ts
@@ -1,43 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { ChallengesModel } from '../../models/challenges.model';
+import { ChallengeModel } from '../../models/challenges.model';
+import { ChallengesService } from '../../services/api/challenges.service';
 
 @Component({
     selector: 'app-calendar',
     templateUrl: './calendar.component.html',
     styleUrl: './calendar.component.scss',
 })
-export class CalendarComponent {
-    challenges: ChallengesModel[] = [
-        {
-            id: 1,
-            name: 'Challenge 1',
-            date: new Date('2024-12-01'),
-            tags: ['tag1', 'tag2'],
-        },
-        {
-            id: 2,
-            name: 'Challenge 2',
-            date: new Date('2024-12-02'),
-            tags: ['tag1'],
-        },
-        {
-            id: 3,
-            name: 'Challenge 3',
-            date: new Date('2024-12-03'),
-            tags: ['tag3'],
-        },
-        {
-            id: 4,
-            name: 'Challenge 4',
-            date: new Date('2024-12-04'),
-            tags: ['tag4'],
-        },
-        {
-            id: 5,
-            name: 'Challenge 5',
-            date: new Date('2024-12-05'),
-            tags: ['tag5', 'tag6'],
-        },
-    ];
+export class CalendarComponent implements OnInit {
+    challenges: ChallengeModel[] = [];
+
+    constructor(private challengesService: ChallengesService) {}
+
+    ngOnInit() {
+        this.getChallenges();
+    }
+
+    getChallenges() {
+        this.challengesService.getChallenges().subscribe(challenges => {
+            this.challenges = challenges;
+        });
+    }
 }
