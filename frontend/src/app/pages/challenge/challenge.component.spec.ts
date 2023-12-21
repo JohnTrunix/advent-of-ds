f9bc29e8c37c0469a9b1a14cd8a3ab4aeeac51e9
JohnTrunix <john@trunix.ch>
2023-12-21 23:19:48 +0100

    test(frontend): fix frontend tests

--- frontend/src/app/pages/challenge/challenge.component.spec.ts
+++ frontend/src/app/pages/challenge/challenge.component.spec.ts
@@ -1,14 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { ChallengeComponent } from './challenge.component';
+import { ChallengesService } from '../../services/api/challenges.service';
 
 describe('ChallengeComponent', () => {
     let component: ChallengeComponent;
     let fixture: ComponentFixture<ChallengeComponent>;
+    let mockActivatedRoute: ActivatedRoute;
+    let mockRouter: Router;
+    let mockChallengesService: ChallengesService;
 
     beforeEach(async () => {
+        mockActivatedRoute = {
+            paramMap: of({ get: () => '123' }),
+        } as never;
+
+        mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+
+        mockChallengesService = jasmine.createSpyObj('ChallengesService', [
+            'getChallenge',
+        ]);
+        (mockChallengesService.getChallenge as jasmine.Spy).and.returnValue(
+            of({
+                uuid: 'your-uuid',
+                day_id: -100,
+                title: 'Test Challenge',
+                tags: ['tag1', 'tag2'],
+                open_at: new Date(),
+                created_by: 'testUser',
+            })
+        );
+
         await TestBed.configureTestingModule({
             declarations: [ChallengeComponent],
+            providers: [
+                { provide: Router, useValue: mockRouter },
+                { provide: ActivatedRoute, useValue: mockActivatedRoute },
+                { provide: ChallengesService, useValue: mockChallengesService },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(ChallengeComponent);
