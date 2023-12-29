1c21287ad882fd55e2b5e830e77b3a55b8f2ea8c
JohnTrunix <john@trunix.ch>
2023-12-29 14:52:14 +0100

    feat(theme): embed theme service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
+
+import { ThemeService } from './services/theme.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Advent of DS';
     isInProduction: boolean = environment.production;
+
+    constructor(private themeService: ThemeService) {}
+
+    ngOnInit(): void {
+        this.themeService.loadTheme();
+    }
 }
