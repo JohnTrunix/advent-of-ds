7917ef5ec5d720b167900cca278f95ebfa342e2d
JohnTrunix <john@trunix.ch>
2023-12-29 17:04:45 +0100

    chore(db): refactor to postgres subfolder

--- db/run.sh
+++
@@ -1,7 +0,0 @@
-# stop and remove container
-docker stop aods-database
-docker rm aods-database
-
-# build and run container
-docker build -t pg-database .
-docker run -p 5432:5432 --name aods-database -d pg-database
