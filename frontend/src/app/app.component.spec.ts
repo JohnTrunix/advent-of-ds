9c11ab34b8bfa663054f9d01f442c7d985e09906
JohnTrunix <john@trunix.ch>
2024-01-08 22:29:04 +0100

    test(frontend): integrate account component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,12 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AccountComponent } from './account/account.component';
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [RouterTestingModule],
-            declarations: [AppComponent, NavbarComponent],
+            declarations: [AppComponent, NavbarComponent, AccountComponent],
         }).compileComponents();
     });
 
