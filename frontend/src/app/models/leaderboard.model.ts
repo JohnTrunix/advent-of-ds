786ef187c357e91e11f8e73de93c682cef9361f8
JohnTrunix <john@trunix.ch>
2024-01-08 20:08:21 +0100

    feat(frontend): add avatar_url to leaderboard view

--- frontend/src/app/models/leaderboard.model.ts
+++ frontend/src/app/models/leaderboard.model.ts
@@ -2,6 +2,7 @@ export interface LeaderboardModel {
     user_uuid: string;
     username: string;
     profile_url: string;
+    avatar_url: string;
     score: number;
     mate: number;
     total: number;
