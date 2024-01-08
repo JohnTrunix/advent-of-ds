eb3798841ce177f645ba7baf038ee7541b64fcd6
JohnTrunix <john@trunix.ch>
2024-01-08 21:24:14 +0100

    test(backend): test user crud schema

--- backend/tests/db/test_schema.py
+++ backend/tests/db/test_schema.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import uuid4
 
-from app.db.schema import Challenge, Leaderboard
+from app.db.schema import Challenge, Leaderboard, User, UserCreate, UserRead
 
 
 def test_challenge():
@@ -42,3 +42,56 @@ def test_leaderboard():
     assert leaderboard.mate == 1
     assert leaderboard.total == 2
     assert leaderboard.ConfigDict.from_attributes
+
+
+def test_user():
+    user = User(
+        username="test",
+        email="test@email.com",
+        avatar_url="https://example.com",
+        profile_url="https://example.com",
+        oauth_token="test",
+    )
+    assert user.username == "test"
+    assert user.email == "test@email.com"
+    assert user.avatar_url == "https://example.com"
+    assert user.profile_url == "https://example.com"
+    assert user.oauth_token == "test"
+    assert user.ConfigDict.from_attributes
+
+
+def test_user_create():
+    user_create = UserCreate(
+        github_id="test",
+        username="test",
+        email="test@email.com",
+        avatar_url="https://example.com",
+        profile_url="https://example.com",
+        oauth_token="test",
+    )
+    assert user_create.github_id == "test"
+    assert user_create.username == "test"
+    assert user_create.email == "test@email.com"
+    assert user_create.avatar_url == "https://example.com"
+    assert user_create.profile_url == "https://example.com"
+    assert user_create.oauth_token == "test"
+    assert user_create.ConfigDict.from_attributes
+
+
+def test_user_read():
+    user_read = UserRead(
+        uuid=uuid4(),
+        github_id="test",
+        username="test",
+        email="test@email.com",
+        avatar_url="https://example.com",
+        profile_url="https://example.com",
+        oauth_token="test",
+    )
+    assert user_read.uuid
+    assert user_read.github_id == "test"
+    assert user_read.username == "test"
+    assert user_read.email == "test@email.com"
+    assert user_read.profile_url == "https://example.com"
+    assert user_read.oauth_token == "test"
+    assert user_read.ConfigDict.from_attributes
